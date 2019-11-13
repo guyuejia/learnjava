c945f34b675293c96522ad2c7a2e36c5ceec17a4
HU <18931177736@189.cn>
2019-11-13 22:31:55 +0800

    hello,world

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -2,7 +2,7 @@
  * @author hushishuai
  * @date 2019/11/13
  */
-public class helloworld {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello,world");
     }
